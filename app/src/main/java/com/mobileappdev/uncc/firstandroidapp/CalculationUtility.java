1f969dd240c1dc6645d5175edce8cd8a12b2b077
Stephen Weber <stephen.weber3@gmail.com>
2018-01-23 21:50:46 -0500

    Add CalculationUtility

---
+++ app/src/main/java/com/mobileappdev/uncc/firstandroidapp/CalculationUtility.java
@@ -0,0 +1,13 @@
+package com.mobileappdev.uncc.firstandroidapp;
+
+/**
+ * Created by Stephen on 1/23/2018.
+ */
+
+public class CalculationUtility {
+
+    protected boolean isAtLeastEighteen(int age){
+        return age >= 18;
+    }
+
+}
